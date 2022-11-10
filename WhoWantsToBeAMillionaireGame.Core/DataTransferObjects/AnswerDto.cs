e2089b3a40cc25054845245b8b214f33d5e197e0
anton-hav <anton.havrilenia@hotmail.com>
2022-11-10 22:41:26 +0100

    add new Answer and Question DTO

---
+++ WhoWantsToBeAMillionaireGame.Core/DataTransferObjects/AnswerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WhoWantsToBeAMillionaireGame.Core.DataTransferObjects;
+
+public class AnswerDto
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; }
+    public bool IsCorrect { get; set; }
+    public Guid QuestionId { get; set; }
+}
---
+++ WhoWantsToBeAMillionaireGame.Core/DataTransferObjects/QuestionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WhoWantsToBeAMillionaireGame.Core.DataTransferObjects;
+
+public class QuestionDto
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; }
+
+    public List<AnswerDto> Answers { get; set; }
+}
