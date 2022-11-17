0832e9a5b55e4c85c7f4e2cd289b7439811cd813
anton-hav <anton.havrilenia@hotmail.com>
2022-11-17 18:03:39 +0100

    add comment to the Game entity

--- WhoWantsToBeAMillionaireGame.DataBase/Entities/Game.cs
+++ WhoWantsToBeAMillionaireGame.DataBase/Entities/Game.cs
@@ -1,5 +1,9 @@
 ﻿namespace WhoWantsToBeAMillionaireGame.DataBase.Entities;
 
+
+// It entity will contain new property like ad datetime of creating,
+// flags for a set of lifelines and etc.
+// Datetime property need for adding cleaning database task to schedule like Hangfire.
 public class Game : IBaseEntity
 {
     public Guid Id { get; set; }
